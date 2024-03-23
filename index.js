944ce36f15adcd7407c3f21de6cdc5c844e38ad6
sandeshjadhav5 <jadhavs932@gmail.com>
2024-03-23 17:28:55 +0530

    added initial setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { connection } = require("./configs/db");
+
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Kisaan Mart");
+});
+
+
+
+
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("connected to the DB");
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(`listening at port : ${process.env.port}`);
+});
