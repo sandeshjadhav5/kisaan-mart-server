84c8c1b7cc2727c0f35d9beeaf5183a7aebf468c
sandeshjadhav5 <jadhavs932@gmail.com>
2024-03-24 09:49:22 +0530

    added product model

---
+++ models/product.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// Define the product schema
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+      },
+      description: String,
+      price: {
+        type: Number,
+        required: true,
+      },
+      category: {
+        type: String,
+        enum: ['Seeds', 'Fertilizers', 'Pesticides', 'Organic', 'Agri Implements',"Gardening","Others"],
+        required: true,
+      },
+      imageUrl: String, // Image URL field
+      rating: {
+        type: Number,
+        default: 0,
+      },
+      reviews: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+          },
+          text: String,
+          rating: {
+            type: Number,
+            enum: [1, 2, 3, 4, 5],
+            required: true,
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
+    }, { timestamps: true });
+
+const ProductModel = mongoose.model('product', productSchema);
+
+module.exports = ProductModel;
